27edd24f14bb8f02d20f11a83a7c24945312ee3a
Bryan De Anda <bryan.deanda1955@alumnos.udg.mx>
2020-10-05 00:30:07 -0500

    prueba 01

--- main.cpp
+++ main.cpp
@@ -3,7 +3,25 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
-
+    string dato;
+    getline(cin, dato);
+    if (dato == "int"){
+        cout << sizeof(int) <<endl;
+    }
+    else if(dato == "char"){
+        cout << sizeof(char) <<endl;
+    }
+    else if(dato == "short"){
+        cout << sizeof(short) <<endl;
+    }
+    else if(dato == "float"){
+        cout << sizeof(float) <<endl;
+    }
+    else if(dato == "double"){
+        cout << sizeof(double) <<endl;
+    }
+    else if(dato == "long"){
+        cout << sizeof(long) <<endl;
+    }
     return 0;
 }
